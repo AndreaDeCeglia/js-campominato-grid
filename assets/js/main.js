37a4622a836744193a95839e0377aaa9f83087ae
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-01 16:55:36 +0100

    second way working progress

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// write the function linked to button's click
+function play(){
+
+    //setting the generation of a uniqe grid, not one each click event
+    document.getElementById("game-container").innerHTML = '';
+
+    //initializzation of choise's value how a Variable
+    let choise = document.getElementById("inputGroupSelect04").ariaValueMax;
+
+    //recalling different parametres depending of the choise
+    switch( choise ){
+
+        case "easy":
+            gridGeneration( 100, "quadrato10");
+            break;
+            
+        case "medium":
+            gridGeneration( 81, "quadrato9");
+            break;
+
+        case "hard":
+            gridGeneration (49, "quadrato7");
+            break;
+            
+    }
+}
+
+function 
